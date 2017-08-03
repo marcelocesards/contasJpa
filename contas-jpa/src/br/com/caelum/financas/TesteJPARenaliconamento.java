717f6b04f453f6f1ee36484799935bf5a49c5564
marce <marce@DESKTOP-BHMQF5G.home>
2017-08-02 23:22:31 -0300

    Criação do relacionamento entre conta e a movimentação.

---
+++ contas-jpa/src/br/com/caelum/financas/TesteJPARenaliconamento.java
@@ -0,0 +1,39 @@
+package br.com.caelum.financas;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteJPARenaliconamento {
+	public static void main(String[] args) {
+		Conta conta  = new Conta();
+		conta.setAgencia("12300");
+		conta.setBanco("itau");
+		conta.setNumero("0077");
+		conta.setTitular("eduardo cunha");
+		
+		Movimentacao movimentacao = new Movimentacao();
+		
+		movimentacao.setData(Calendar.getInstance());
+		movimentacao.setEscricao("churrascaria");
+		movimentacao.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao.setValor(new BigDecimal("200.00"));
+		movimentacao.setConta(conta);
+		EntityManager manager = new JPAUtil().getEntityManager();
+		
+		manager.getTransaction().begin();
+
+		manager.persist(conta);
+		manager.persist(movimentacao);
+		
+		manager.getTransaction().commit();
+		manager.close();
+	}
+
+}
